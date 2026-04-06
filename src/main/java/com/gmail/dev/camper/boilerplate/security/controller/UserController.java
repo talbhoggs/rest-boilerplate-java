7c8e3d4d7bec0e03f0adeea3ecd2440046ed0f04
charles amper <amperca@ph.ibm.com>
2026-04-06 19:06:47 +0800

    add Spotless plugin for code formatting

--- src/main/java/com/gmail/dev/camper/boilerplate/security/controller/UserController.java
+++ src/main/java/com/gmail/dev/camper/boilerplate/security/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.gmail.dev.camper.boilerplate.security.controller;
 
+import com.gmail.dev.camper.boilerplate.security.entity.User;
+import com.gmail.dev.camper.boilerplate.security.repository.UserRepository;
 import java.util.Optional;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,38 +10,34 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.gmail.dev.camper.boilerplate.security.entity.User;
-import com.gmail.dev.camper.boilerplate.security.repository.UserRepository;
-
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-   
-    private final PasswordEncoder encoder;
-    private final UserRepository userRepository;
 
-    public UserController(PasswordEncoder encoder, UserRepository userRepository) {
-        this.encoder = encoder;
-        this.userRepository = userRepository;
+  private final PasswordEncoder encoder;
+  private final UserRepository userRepository;
+
+  public UserController(PasswordEncoder encoder, UserRepository userRepository) {
+    this.encoder = encoder;
+    this.userRepository = userRepository;
+  }
+
+  @PostMapping
+  public ResponseEntity<String> createUser(@RequestBody User user) {
+
+    // refactor code
+    // error handling
+    //
+    Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
+
+    if (!existingUser.isPresent()) {
+      User newUser = new User();
+      newUser.setUsername(user.getUsername());
+      newUser.setPassword(encoder.encode(user.getPassword()));
+      userRepository.save(newUser);
+      return ResponseEntity.ok("User Created Successfully");
     }
 
-    @PostMapping
-    public ResponseEntity<String> createUser(@RequestBody User user) {
-
-
-        // refactor code
-        // error handling
-        // 
-        Optional<User> existingUser = userRepository.findByUsername(user.getUsername());
-
-        if(!existingUser.isPresent()) {
-            User newUser = new User();
-            newUser.setUsername(user.getUsername());
-            newUser.setPassword(encoder.encode(user.getPassword()));
-            userRepository.save(newUser);
-            return ResponseEntity.ok("User Created Successfully");
-        }
-
-        return ResponseEntity.badRequest().body("User Created Successfully");
-    }
+    return ResponseEntity.badRequest().body("User Created Successfully");
+  }
 }
--- src/main/java/com/gmail/dev/camper/boilerplate/security/repository/UserRepository.java
+++ src/main/java/com/gmail/dev/camper/boilerplate/security/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package com.gmail.dev.camper.boilerplate.security.repository;
 
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.gmail.dev.camper.boilerplate.security.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
-    
+  Optional<User> findByUsername(String username);
 }
