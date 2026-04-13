d843eb1bfbaa44b0cd6847840366f4bc0f91ac13
charles amper <amperca@ph.ibm.com>
2026-04-13 10:31:50 +0800

    standardize unauthorized/forbidden responses to JSON

--- src/main/java/com/gmail/dev/camper/boilerplate/security/component/JwtAuthenticationEntryPoint.java
+++ src/main/java/com/gmail/dev/camper/boilerplate/security/component/JwtAuthenticationEntryPoint.java
@@ -1,9 +1,15 @@
 package com.gmail.dev.camper.boilerplate.security.component;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gmail.dev.camper.boilerplate.exceptions.ErrorResponse;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -17,9 +23,20 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
       HttpServletResponse response,
       AuthenticationException authException)
       throws IOException, ServletException {
-    System.out.println("Access unauthorized");
-    response.sendError(
-        HttpServletResponse.SC_UNAUTHORIZED,
-        "Authentication failed: " + authException.getMessage());
+
+    Map<String, String> exceptionMessages = new HashMap<>();
+    exceptionMessages.put("errorMessage", authException.getMessage());
+
+    ErrorResponse errorResponse =
+        new ErrorResponse(
+            new Date(),
+            HttpStatus.UNAUTHORIZED.value(),
+            HttpStatus.UNAUTHORIZED.getReasonPhrase(),
+            exceptionMessages);
+
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+    response.setContentType("application/json");
+
+    new ObjectMapper().writeValue(response.getOutputStream(), errorResponse);
   }
 }
